91a7b31c3a73784d4c135604569fc8def6553e03
Tim-Hedlund <tim.hedlund@elev.ga.ntig.se>
2024-05-13 09:02:31 +0200

    börjat

--- src/MulticastChatClient.java
+++ src/MulticastChatClient.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class MulticastChatClient {
+import java.net.*;
+import java.io.*;
+
+public class MulticastChatClient {
+
+    public static void main(String [] args) throws Exception{
+
+        int portNumber = 55623;
+
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        MulticastSocket chatMulticastSocket = new MulticastSocket(portNumber);
+
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        chatMulticastSocket.joinGroup(group);
+
+        String msg = "";
+
+        System.out.println("Type a message for the server: ");
+
+
+    }
+
 }
